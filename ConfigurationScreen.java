4dee675fd69d84dd8703fd4243047be73bec5242
akvh3 <andrewvon@gatech.edu>
2019-09-20 12:21:04 -0400

    Changed the files.

--- ConfigurationScreen.java
+++ ConfigurationScreen.java
@@ -1,6 +1,10 @@
+import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Dictionary;
+
 public class ConfigurationScreen extends JFrame implements ActionListener, WindowListener {
     /*
     * OKKKK so this is the configurationScreen
@@ -17,6 +21,10 @@ public class ConfigurationScreen extends JFrame implements ActionListener, Windo
     private static JTextField userEntry;
     private static JTextArea characters;
     private static JButton confirmButton;
+    private static ButtonGroup group;
+    private static JPanel configPanel = new JPanel();
+    private static int credits;
+    private static int skillPoints = 0;
 
     public ConfigurationScreen(String title) {
         configFrame = new JFrame(title);
@@ -36,12 +44,16 @@ public class ConfigurationScreen extends JFrame implements ActionListener, Windo
         confirmButton = new JButton("Confirm");
 
         JPanel pane = makeWelcomePane();
-
         configFrame.add(pane);
         configFrame.add(makeTextPane("Configure Your Character"));
-
+        configPanel.setLayout(new GridLayout(15, 15, 5, 0));
+        configFrame.add(addLabel("Enter your name below:"));
         configFrame.add(makeTextField());
-
+        configFrame.add(addLabel("Choose your difficulty:"));
+        configFrame.add(addRadioButton());
+        configFrame.add(addLabel("Allocate your skill points: " + skillPoints + " points total"));
+        configFrame.add(addSliders());
+        configFrame.add(addGoodButton("Confirm"));
         configFrame.pack();
         configFrame.setVisible(true);
 
@@ -55,12 +67,59 @@ public class ConfigurationScreen extends JFrame implements ActionListener, Windo
         });
     }
 
-    public static JTextField makeTextField() {
+    public static JPanel addLabel(String label) {
+        JLabel words = new JLabel(label);
+        configPanel.add(words);
+        return configPanel;
+    }
+
+    public static JPanel addSliders() {
+        JLabel lPilot = new JLabel("Pilot:");
+        JSlider pilot = new JSlider();
+        JLabel lFighter = new JLabel("Fighter:");
+        JSlider fighter = new JSlider();
+        JLabel lMerchant = new JLabel("Merchant:");
+        JSlider merchant = new JSlider();
+        JLabel lEngineer = new JLabel("Engineer:");
+        JSlider engineer = new JSlider();
+
+        configPanel.add(lPilot);
+        configPanel.add(pilot);
+        configPanel.add(lFighter);
+        configPanel.add(fighter);
+        configPanel.add(lMerchant);
+        configPanel.add(merchant);
+        configPanel.add(lEngineer);
+        configPanel.add(engineer);
+
+        return configPanel;
+    }
+
+    public static JPanel addRadioButton() {
+        JRadioButton easyButton = new JRadioButton("Easy");
+        JRadioButton medButton = new JRadioButton("Medium");
+        JRadioButton hardButton = new JRadioButton("Hard");
+
+        group = new ButtonGroup();
+        group.add(easyButton);
+        group.add(medButton);
+        group.add(hardButton);
+
+        JPanel radioPanel = new JPanel(new GridLayout(1, 0));
+        radioPanel.add(easyButton);
+        radioPanel.add(medButton);
+        radioPanel.add(hardButton);
+
+        configPanel.add(radioPanel);
+        return configPanel;
+    }
+
+    public static JPanel makeTextField() {
         userEntry = new JTextField(15);
         TextFieldListener tfListen = new TextFieldListener();
         userEntry.addActionListener(tfListen);
-
-        return userEntry;
+        configPanel.add(userEntry);
+        return configPanel;
     }
 
     private static class TextFieldListener implements ActionListener {
@@ -78,6 +137,11 @@ public class ConfigurationScreen extends JFrame implements ActionListener, Windo
         return p;
     }
 
+    public static JPanel addGoodButton(String label) {
+        JButton button = new JButton(label);
+        configPanel.add(button);
+        return configPanel;
+    }
     public static JPanel makeTextPane(String name) {
         JPanel textP = new JPanel();
         addText(name, textP);
